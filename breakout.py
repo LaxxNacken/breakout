6672eebe54bf03fee943f8f67a86203f5356db2e
LaxxNacken <j.s.laxxnacken@gmail.com>
2022-08-18 15:58:51 +0200

    half done ig

--- breakout.py
+++ breakout.py
@@ -5,9 +5,16 @@ window_width = 500
 window_height = 500
 
 
-global paddle_position
-paddle_position = [210, 450] 
+global block_position1
+block_position1= [200, 150]
+global block_position2
+block_position2 = [250, 150]
+global paddle_position1
+paddle_position1 = [210, 490] 
+global paddle_position2
+paddle_position2 = [290, 490]
 ball_position = [250, 250]
+ball_move = [2, 2]
 
 pygame.init()
 pygame.joystick.init()
@@ -17,24 +24,28 @@ window = pygame.display.set_mode((window_width, window_height))
 
 
 def ball_collision():
-    global ball_position
-    if ball_position[1] == paddle_position[1] and ball_position[0] == ball_position[0]:
-        ball_position[0] *= -1
-        ball_position[1] *= -1
-
+    global ball_move
+    if ball_position[1] > paddle_position1[1] - 10 and ball_position[0] >  paddle_position1[0] and ball_position[0] < paddle_position2[0]:
+        ball_move[1]  *= -1 
+    if ball_position[0] > window_width - 10 or ball_position[0] < 0 + 10:
+        ball_move[0] *= -1
+    if ball_position[1] < 0 + 10:
+        ball_move[1] *= -1
+    if ball_position[1] > block_position1[1] - 10 and ball_position[0] > block_position1[0] and ball_position[0] < block_position2[0]:
+        ball_move[1] *= -1 
 
 def ball_movement():
-    ball_position[0] += 2
-    ball_position[1] += 2
-    print(ball_position)
+    ball_position[0] = ball_position[0] + ball_move[0]
+    ball_position[1] = ball_position[1] + ball_move[1]
 
 def draw_stuff():
     window.fill(pygame.Color(0, 0, 0))
-    pygame.draw.rect(window, pygame.Color(0, 0, 255), (paddle_position[0], paddle_position[1], 80, 10))
-    pygame.draw.circle(window, pygame.Color(255, 0, 0),(ball_position[0], ball_position[1]), int(10))    
+    pygame.draw.rect(window, pygame.Color(0, 0, 255), (paddle_position1[0], paddle_position1[1], 80, 10))
+    pygame.draw.circle(window, pygame.Color(255, 0, 0),(int(ball_position[0]),int( ball_position[1])), int(10))
+    pygame.draw.rect(window, pygame.Color(255, 255, 255), (block_position1[0], block_position1[1] , 80, 10))
 
 def game_over():
-    if ball_position[1] > window_height - 10:
+    if ball_position[1] > 490:
         game_over_message()
 
 def game_over_message():
@@ -64,11 +75,13 @@ def game_loop():
                  axis = joystick.get_axis(i)
                  if i == 0:
                      if axis > 0.0:
-                        paddle_position[0] += 10
+                        paddle_position1[0] += 10
+                        paddle_position2[0] += 10 
                      if axis < 0.0:
-                        paddle_position[0] -= 10
-        ball_collision()
+                        paddle_position1[0] -= 10
+                        paddle_position2[0] -= 10 
         ball_movement()
+        ball_collision()
         game_over()
         draw_stuff()
         pygame.display.update()
